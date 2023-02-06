501b49300e01bd209fa33a6e3a1131dfde2428df
Roman.Kucherenko <Roman.Kucherenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2023-02-06 06:46:40 +0000

    CKeyDataProviderComp.cpp cleaned
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@5898 21205d9f-8918-2240-a3d5-c39064a2c852

--- Include/prolifedata/CKeyDataProviderComp.cpp
+++ Include/prolifedata/CKeyDataProviderComp.cpp
@@ -26,7 +26,7 @@ namespace prolifedata
 bool CKeyDataProviderComp::GetData(QByteArray& data, const QByteArray& dataId) const
 {
 	if (!m_objectCollectionCompPtr.IsValid()){
-        SendCriticalMessage(0, "objectCollectionCompPtr is not valid", "Server data provider");
+		SendCriticalMessage(0, "objectCollectionCompPtr is not valid", "Server data provider");
 
 		return false;
 	}
@@ -34,7 +34,7 @@ bool CKeyDataProviderComp::GetData(QByteArray& data, const QByteArray& dataId) c
 	QByteArrayList ids = dataId.split('/');
 
 	if (ids.size() != 2){
-        SendCriticalMessage(0, "Number of data Id not equal 2", "Server data provider");
+		SendCriticalMessage(0, "Number of data Id not equal 2", "Server data provider");
 
 		return false;
 	}
@@ -43,175 +43,171 @@ bool CKeyDataProviderComp::GetData(QByteArray& data, const QByteArray& dataId) c
 	QByteArray productObjectId = ids[1];
 
 	imtbase::IObjectCollection::DataPtr dataPtr;
-    if (!m_objectCollectionCompPtr->GetObjectData(orderId, dataPtr)){
-        SendCriticalMessage(0, "Don't get data pointer for order id: " + orderId, "Server data provider");
+	if (!m_objectCollectionCompPtr->GetObjectData(orderId, dataPtr)){
+		SendCriticalMessage(0, "Don't get data pointer for order id: " + orderId, "Server data provider");
 
-        return false;
-    }
+		return false;
+	}
 
-    prolifedata::IOrderInfo* orderPtr = dynamic_cast<prolifedata::IOrderInfo*>(dataPtr.GetPtr());
-    if (orderPtr == nullptr){
-        SendCriticalMessage(0, "OrderInfo pointer is null: " + orderId, "Server data provider");
+	prolifedata::IOrderInfo* orderPtr = dynamic_cast<prolifedata::IOrderInfo*>(dataPtr.GetPtr());
+	if (orderPtr == nullptr){
+		SendCriticalMessage(0, "OrderInfo pointer is null: " + orderId, "Server data provider");
 
-        return false;
-    }
+		return false;
+	}
 
-    imtbase::IObjectCollection* productCollectionPtr = orderPtr->GetProducts();
-    if (productCollectionPtr == nullptr){
-        SendCriticalMessage(0, "Order don't contain prodicts: " + orderId, "Server data provider");
+	imtbase::IObjectCollection* productCollectionPtr = orderPtr->GetProducts();
+	if (productCollectionPtr == nullptr){
+		SendCriticalMessage(0, "Order don't contain prodicts: " + orderId, "Server data provider");
 
-        return false;
-    }
+		return false;
+	}
 
-    imtbase::IObjectCollection::DataPtr hardwareDataPtr;
-    if (!productCollectionPtr->GetObjectData(productObjectId, hardwareDataPtr)){
-        SendCriticalMessage(0, "Don't get hardware data pointer for id: " + productObjectId, "Server data provider");
+	imtbase::IObjectCollection::DataPtr hardwareDataPtr;
+	if (!productCollectionPtr->GetObjectData(productObjectId, hardwareDataPtr)){
+		SendCriticalMessage(0, "Don't get hardware data pointer for id: " + productObjectId, "Server data provider");
 
-        return false;
-    }
+		return false;
+	}
 
-    imtlic::CLicensedHardwareInstanceInfo* hardwareInstancePtr = dynamic_cast<imtlic::CLicensedHardwareInstanceInfo*>(hardwareDataPtr.GetPtr());
-    if (hardwareInstancePtr == nullptr){
-        SendCriticalMessage(0, "Hardware instance error", "Server data provider");
+	imtlic::CLicensedHardwareInstanceInfo* hardwareInstancePtr = dynamic_cast<imtlic::CLicensedHardwareInstanceInfo*>(hardwareDataPtr.GetPtr());
+	if (hardwareInstancePtr == nullptr){
+		SendCriticalMessage(0, "Hardware instance error", "Server data provider");
 
-        return false;
-    }
+		return false;
+	}
 
+	QByteArray softwareInstanceId = hardwareInstancePtr->GetSoftwareId();
+	imtbase::IObjectCollection::DataPtr softwareDataPtr;
+	if (!productCollectionPtr->GetObjectData(softwareInstanceId, softwareDataPtr)){
+		SendCriticalMessage(0, "Don't get data object pointer for id: " + softwareInstanceId, "Server data provider");
 
-    QByteArray softwareInstanceId = hardwareInstancePtr->GetSoftwareId();
-    imtbase::IObjectCollection::DataPtr softwareDataPtr;
-    if (!productCollectionPtr->GetObjectData(softwareInstanceId, softwareDataPtr)){
-        SendCriticalMessage(0, "Don't get data object pointer for id: " + softwareInstanceId, "Server data provider");
+		return false;
+	}
 
-        return false;
-    }
+	imtlic::IProductInstanceInfo* productInstancePtr = dynamic_cast<imtlic::IProductInstanceInfo*>(softwareDataPtr.GetPtr());
+	if (productInstancePtr == nullptr){
+		SendCriticalMessage(0, "Software instance error: " + softwareInstanceId, "Server data provider");
 
-    imtlic::IProductInstanceInfo* productInstancePtr = dynamic_cast<imtlic::IProductInstanceInfo*>(softwareDataPtr.GetPtr());
-    if (productInstancePtr == nullptr){
-        SendCriticalMessage(0, "Software instance error: " + softwareInstanceId, "Server data provider");
+		return false;
+	}
 
-        return false;
-    }
+	QByteArray instanceId = hardwareInstancePtr->GetProductInstanceId();
+	QByteArray productId = productInstancePtr->GetProductId();
+	QByteArray customerId = orderPtr->GetCustomerId();
+	productInstancePtr->SetupProductInstance(productId, instanceId, customerId);
 
-    QByteArray instanceId = hardwareInstancePtr->GetProductInstanceId();
-    QByteArray productId = productInstancePtr->GetProductId();
-    QByteArray customerId = orderPtr->GetCustomerId();
-    productInstancePtr->SetupProductInstance(productId, instanceId, customerId);
+	if (!m_gqlLicenseRequestCompPtr.IsValid()){
+		SendCriticalMessage(0, "gqlLicenseRequestCompPtr is not valid", "Server data provider");
 
-    if (!m_gqlLicenseRequestCompPtr.IsValid()){
-        SendCriticalMessage(0, "gqlLicenseRequestCompPtr is not valid", "Server data provider");
+		return false;
+	}
 
-        return false;
-    }
+	imtgql::CGqlRequest gqlRequest(imtgql::CGqlRequest::RT_QUERY, "ProductItem");
+	imtgql::CGqlObject queryFields("item");
+	queryFields.InsertField("Id");
+	gqlRequest.AddField(queryFields);
 
-    imtgql::CGqlRequest gqlRequest(imtgql::CGqlRequest::RT_QUERY, "ProductItem");
-    imtgql::CGqlObject queryFields("item");
-    queryFields.InsertField("Id");
-    gqlRequest.AddField(queryFields);
+	imtgql::CGqlObject inputParams("input");
+	inputParams.InsertField(QByteArray("Id"), QVariant(productId));
+	gqlRequest.AddParam(inputParams);
 
-    imtgql::CGqlObject inputParams("input");
-    inputParams.InsertField(QByteArray("Id"), QVariant(productId));
-    gqlRequest.AddParam(inputParams);
+	QString errorMessage;
+	imtbase::CTreeItemModel* productModelPtr = m_gqlLicenseRequestCompPtr->CreateResponse(gqlRequest, errorMessage);
+	if (productModelPtr == nullptr){
+		SendCriticalMessage(0, "Don't create response: " + errorMessage, "Server data provider");
 
-    QString errorMessage;
-    imtbase::CTreeItemModel* productModelPtr = m_gqlLicenseRequestCompPtr->CreateResponse(gqlRequest, errorMessage);
-    if (productModelPtr == nullptr){
-        SendCriticalMessage(0, "Don't create response: " + errorMessage, "Server data provider");
+		return false;
+	}
 
-        return false;
-    }
+	imtbase::CTreeItemModel* dataModelPtr = productModelPtr->GetTreeItemModel("data");
+	if (dataModelPtr == nullptr){
+		SendCriticalMessage(0, "No date in product: " + productId, "Server data provider");
 
-    imtbase::CTreeItemModel* dataModelPtr = productModelPtr->GetTreeItemModel("data");
-    if (dataModelPtr == nullptr){
-        SendCriticalMessage(0, "No date in product: " + productId, "Server data provider");
+		return false;
+	}
 
-        return false;
-    }
+	imtbase::CTreeItemModel* licensesModelPtr = dataModelPtr->GetTreeItemModel("Features");
+	if (licensesModelPtr == nullptr){
+		SendCriticalMessage(0, "No features in product: " + productId, "Server data provider");
 
-    imtbase::CTreeItemModel* licensesModelPtr = dataModelPtr->GetTreeItemModel("Features");
-    if (licensesModelPtr == nullptr){
-        SendCriticalMessage(0, "No features in product: " + productId, "Server data provider");
+		return false;
+	}
 
-        return false;
-    }
+	const imtbase::ICollectionInfo& licenseList = productInstancePtr->GetLicenseInstances();
 
-    qDebug() << productModelPtr->toJSON();
-    productInstancePtr->GetLicenseInstances();
-    const imtbase::ICollectionInfo& licenseList = productInstancePtr->GetLicenseInstances();
+	imtbase::ICollectionInfo::Ids licenseIds = licenseList.GetElementIds();
+	for (const QByteArray& licenseId : licenseIds){
+		imtlic::ILicenseInstance* licenseInstancePtr = dynamic_cast<imtlic::ILicenseInstance*>( const_cast<imtlic::ILicenseInstance*>( productInstancePtr->GetLicenseInstance(licenseId)));
+		if (licenseInstancePtr == nullptr){
+			SendCriticalMessage(0, "License instance error: " + licenseId, "Server data provider");
 
-    imtbase::ICollectionInfo::Ids licenseIds = licenseList.GetElementIds();
-    for (const QByteArray& licenseId : licenseIds){
-        imtlic::ILicenseInstance* licenseInstancePtr = dynamic_cast<imtlic::ILicenseInstance*>( const_cast<imtlic::ILicenseInstance*>( productInstancePtr->GetLicenseInstance(licenseId)));
-        if (licenseInstancePtr == nullptr){
-            SendCriticalMessage(0, "License instance error: " + licenseId, "Server data provider");
+			return false;
+		}
 
-            return false;
-        }
+		if (!licensesModelPtr->ContainsKey(licenseId)){
+			SendCriticalMessage(0, "The product does not contain a license " + licenseId, "Server data provider");
 
-        if (!licensesModelPtr->ContainsKey(licenseId)){
-            SendCriticalMessage(0, "The product does not contain a license " + licenseId, "Server data provider");
+			return false;
+		}
 
-            return false;
-        }
+		imtbase::CTreeItemModel* featuresModelPtr = licensesModelPtr->GetTreeItemModel(licenseId);
+		if (featuresModelPtr == nullptr){
+			SendCriticalMessage(0, "Future model pointer is null " + licenseId, "Server data provider");
 
-        imtbase::CTreeItemModel* featuresModelPtr = licensesModelPtr->GetTreeItemModel(licenseId);
-        if (featuresModelPtr == nullptr){
-            SendCriticalMessage(0, "Future model pointer is null " + licenseId, "Server data provider");
+			return false;
+		}
 
-            return false;
-        }
+		imtlic::ILicenseInfo::FeatureInfos featureInfos;
+		for (int featureIndex = 0; featureIndex < featuresModelPtr->GetItemsCount(); featureIndex++){
+			imtlic::ILicenseInfo::FeatureInfo featureInfo;
+			featureInfo.name = featuresModelPtr->GetData("Name", featureIndex).toString();
+			featureInfo.id = featuresModelPtr->GetData("Id", featureIndex).toByteArray();
+			featureInfos.append(featureInfo);
+		}
 
-        imtlic::ILicenseInfo::FeatureInfos featureInfos;
-        for (int featureIndex = 0; featureIndex < featuresModelPtr->GetItemsCount(); featureIndex++){
-            imtlic::ILicenseInfo::FeatureInfo featureInfo;
-            featureInfo.name = featuresModelPtr->GetData("Name", featureIndex).toString();
-            featureInfo.id = featuresModelPtr->GetData("Id", featureIndex).toByteArray();
-            featureInfos.append(featureInfo);
-        }
+		licenseInstancePtr->SetFeatureInfos(featureInfos);
+		imtbase::CTreeItemModel* licensesItemsModelPtr = dataModelPtr->GetTreeItemModel("Items");
+		if (licensesItemsModelPtr == nullptr){
+			SendCriticalMessage(0, "Items model pointer is null ", "Server data provider");
 
-        licenseInstancePtr->SetFeatureInfos(featureInfos);
-        imtbase::CTreeItemModel* licensesItemsModelPtr = dataModelPtr->GetTreeItemModel("Items");
-        if (licensesItemsModelPtr == nullptr){
-            SendCriticalMessage(0, "Items model pointer is null ", "Server data provider");
+			return false;
+		}
 
-            return false;
-        }
+		for (int itemIndex = 0; itemIndex < licensesItemsModelPtr->GetItemsCount(); itemIndex++){
+			if (licensesItemsModelPtr->GetData("Id", itemIndex).toByteArray() == licenseId){
+				licenseInstancePtr->SetLicenseName(licensesItemsModelPtr->GetData("Name", itemIndex).toByteArray());
+			}
+		}
+	}
 
-        for (int itemIndex = 0; itemIndex < licensesItemsModelPtr->GetItemsCount(); itemIndex++){
-            if (licensesItemsModelPtr->GetData("Id", itemIndex).toByteArray() == licenseId){
-                licenseInstancePtr->SetLicenseName(licensesItemsModelPtr->GetData("Name", itemIndex).toByteArray());
-            }
-        }
-    }
+	m_productInstanceId = instanceId;
 
+	QTemporaryDir tempDir;
+	QString filePathTmp = tempDir.path() + "/" + QUuid::createUuid().toString() + ".xml";
 
-    m_productInstanceId = instanceId;
+	int state = m_licensePersistenceCompPtr->SaveToFile(*productInstancePtr, filePathTmp);
+	if (state != ifile::IFilePersistence::OS_OK){
+		SendErrorMessage(0, "License file could not be saved", "Server data provider");
 
-    QTemporaryDir tempDir;
-    QString filePathTmp = tempDir.path() + "/" + QUuid::createUuid().toString() + ".xml";
+		return false;
+	}
 
-    int state = m_licensePersistenceCompPtr->SaveToFile(*productInstancePtr, filePathTmp);
-    if (state != ifile::IFilePersistence::OS_OK){
-        SendErrorMessage(0, "License file could not be saved", "Server data provider");
+	QFile file(filePathTmp);
 
-        return false;
-    }
+	if (!file.open(QIODevice::ReadOnly )){
+		SendErrorMessage(0, "License file could not be opened", "Server data provider");
 
-    QFile file(filePathTmp);
+		return false;
+	}
 
-    if (!file.open(QIODevice::ReadOnly )){
-        SendErrorMessage(0, "License file could not be opened", "Server data provider");
+	data = file.readAll();
 
-        return false;
-    }
+	file.close();
 
-    data = file.readAll();
+	SendInfoMessage(0, QString("License file for product %1 successfully created").arg(QString(productId)), "Server data provider");
 
-    file.close();
-
-    SendInfoMessage(0, QString("License file for product %1 successfully created").arg(QString(productId)), "Server data provider");
-
-    return true;
+	return true;
 }
 
 
