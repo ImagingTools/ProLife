a362d4a398ff6bc1213942a87f8bed4db1fab6b0
Victor.Shcherbina <Victor.Shcherbina@21205d9f-8918-2240-a3d5-c39064a2c852>
2023-02-10 11:21:18 +0000

    Migration db to ver_2
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@5947 21205d9f-8918-2240-a3d5-c39064a2c852

---
+++ Build/Migrations/migration_2.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "Devices"(
+    DeviceId VARCHAR (1000) NOT NULL,
+    DeviceName VARCHAR (1000) NOT NULL, 
+    MacAddress VARCHAR (1000) NOT NULL,
+    SerialNumber VARCHAR (1000) NOT NULL,
+    PRIMARY KEY (DeviceId)
+);
