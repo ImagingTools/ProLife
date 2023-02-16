a356295b29054fa5759cf654fbad3221a839ee03
Victor.Shcherbina <Victor.Shcherbina@21205d9f-8918-2240-a3d5-c39064a2c852>
2023-02-16 12:23:47 +0000

    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@5997 21205d9f-8918-2240-a3d5-c39064a2c852

--- Build/Migrations/migration_0.sql
+++ Build/Migrations/migration_0.sql
@@ -128,3 +128,15 @@ CREATE TABLE "Orders"(
     FOREIGN KEY (AccountId) REFERENCES "Accounts"(Id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+CREATE TABLE "Sensors"(
+    Id SERIAL,
+    SensorId VARCHAR (1000) NOT NULL,
+    OrderId VARCHAR (1000) NOT NULL,
+    Document JSONB,
+    RevisionNumber BIGINT,
+    LastModified TIMESTAMP,
+    Checksum BIGINT,
+    IsActive BOOLEAN,
+    PRIMARY KEY (Id)
+);
+
