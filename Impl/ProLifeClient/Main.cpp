43fe174fc1cafa503a935caa4236b46423342cec
Roman.Kucherenko <Roman.Kucherenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-09-11 12:36:25 +0000

    Lisa client Main.cpp corrected; ProLife client Main.cpp corrected;
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@11101 21205d9f-8918-2240-a3d5-c39064a2c852

--- Impl/ProLifeClient/Main.cpp
+++ Impl/ProLifeClient/Main.cpp
@@ -1,23 +1,5 @@
-// Qt includes
-#include <QtCore/QDir>
-#include <QtCore/QCoreApplication>
-#include <QtWidgets/QApplication>
-#include <QtWidgets/QStyleFactory>
-#include <QtQml/QQmlEngine>
-#include <QtQml/QQmlEngine>
-#include <QtQml/QtQml>
-#include <QtQuickControls2/QQuickStyle>
-
-// ACF includes
-#include <ibase/IApplication.h>
-
 // ImtCore includes
-#include <imtstyle/CImtStyle.h>
-#include <imtbase/CTreeItemModel.h>
-#include <imtqml/CGqlModel.h>
-#include <imtqml/CGqlRequest.h>
-#include <imtqml/CQuickApplicationComp.h>
-#include <imtqml/CRemoteFileController.h>
+#include <imtbase/Init.h>
 
 // ProLife includes
 #include <GeneratedFiles/ProLifeClient/CProLifeClient.h>
@@ -25,27 +7,8 @@
 
 int main(int argc, char *argv[])
 {
-	Q_INIT_RESOURCE(imtauthguiqml);
-	Q_INIT_RESOURCE(imtguigqlqml);
 	Q_INIT_RESOURCE(imtlicguiqml);
-	Q_INIT_RESOURCE(imtguiqml);
-	Q_INIT_RESOURCE(imtcolguiqml);
-	Q_INIT_RESOURCE(imtdocguiqml);
-	Q_INIT_RESOURCE(imtresthtml);
-
-	Q_INIT_RESOURCE(ImtCoreLoc);
-	Q_INIT_RESOURCE(ProLifeLoc);
-
-	Q_INIT_RESOURCE(imtstylecontrolsqml);
-	Q_INIT_RESOURCE(imtcontrolsqml);
-
-	Q_INIT_RESOURCE(imtauthguiTheme);
-	Q_INIT_RESOURCE(imtguiTheme);
 	Q_INIT_RESOURCE(imtlicguiTheme);
-
-	Q_INIT_RESOURCE(imtauthGroupsSdl);
-	Q_INIT_RESOURCE(imtauthRolesSdl);
-	Q_INIT_RESOURCE(imtauthUsersSdl);
 
 	Q_INIT_RESOURCE(prolifeqml);
 	Q_INIT_RESOURCE(prolifestyle);
@@ -54,32 +17,11 @@ int main(int argc, char *argv[])
 	Q_INIT_RESOURCE(prolifeLicensesSdl);
 	Q_INIT_RESOURCE(prolifeOrdersSdl);
 
-	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	Q_INIT_RESOURCE(imtlicFeaturesSdl);
+	Q_INIT_RESOURCE(imtlicProductsSdl);
+	Q_INIT_RESOURCE(imtlicLicensesSdl);
 
-	CProLifeClient instance;
-
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
-	qmlRegisterModule("QtGraphicalEffects", 1, 12);
-	qmlRegisterModule("QtGraphicalEffects", 1, 0);
-	qmlRegisterModule("QtQuick.Dialogs", 1, 3);
-#else
-	qmlRegisterModule("QtQuick.Dialogs", 6, 2);
-	qmlRegisterModule("Qt5Compat.GraphicalEffects", 6, 0);
-#endif
-
-//	QQuickStyle::setStyle("Material");
-
-	qmlRegisterType<imtbase::CTreeItemModel>("Acf", 1, 0, "TreeItemModel");
-	qmlRegisterType<imtqml::CGqlModel>("Acf", 1, 0, "GqlModel");
-	qmlRegisterType<imtqml::CGqlRequest>("Acf", 1, 0, "GqlRequest");
-	qmlRegisterType<imtqml::CRemoteFileController>("Acf", 1, 0, "RemoteFileController");
-
-	ibase::IApplication* applicationPtr = instance.GetInterface<ibase::IApplication>();
-	if (applicationPtr != nullptr){
-		return applicationPtr->Execute(argc, argv);
-	}
-
-	return -1;
+	return Run<CProLifeClient, DefaultImtCoreQmlInitializer>(argc, argv);
 }
 
 
