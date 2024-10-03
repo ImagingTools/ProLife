5f62d857d36254884925003f2352e5f820e7f7a2
Roman.Kucherenko <Roman.Kucherenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2024-10-03 04:12:35 +0000

    Code cleanup; Partitura corrected;
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@11314 21205d9f-8918-2240-a3d5-c39064a2c852

--- Impl/ProLifeServerConfigurator/Main.cpp
+++ Impl/ProLifeServerConfigurator/Main.cpp
@@ -1,5 +1,6 @@
 // ImtCore includes
 #include <imtbase/Init.h>
+#include <imtqml/CQmlProcess.h>
 
 // ProLife includes
 #include <GeneratedFiles/ProLifeServerConfigurator/CProLifeServerConfigurator.h>
@@ -9,6 +10,7 @@ int main(int argc, char *argv[])
 {
 	Q_INIT_RESOURCE(ProLifeServerConfigurator);
 	Q_INIT_RESOURCE(prolifeqml);
+	qmlRegisterType<imtqml::CQmlProcess>("imtqml", 1, 0, "Process");
 	return Run<CProLifeServerConfigurator, DefaultImtCoreQmlInitializer>(argc, argv);
 }
 
