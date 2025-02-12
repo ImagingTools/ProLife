62a5150b00463d01a9843ab4d4b2d04c72303496
Roman.Kucherenko <Roman.Kucherenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-02-12 06:10:56 +0000

    ProLife test data updated
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@12972 21205d9f-8918-2240-a3d5-c39064a2c852

--- Tests/TestRunner.bat
+++ Tests/TestRunner.bat
@@ -2,49 +2,55 @@
 
 set BUILD_DIR=%1
 
-set PROLIFE_BACKUP_FILE="%BUILD_DIR%\ProLife\Tests\prolifetest.backup"
-set LISA_BACKUP_FILE="%BUILD_DIR%\ProLife\Tests\lisatest.backup"
-set PUMA_BACKUP_FILE="%BUILD_DIR%\ProLife\Tests\pumatest.backup"
-set PROLIFE_DB_NAME="prolifetest"
-set LISA_DB_NAME="lisatest"
-set PUMA_DB_NAME="pumatest"
-set PG_USER="postgres"
-set PG_HOST="localhost"
-set PG_PORT="5432"
+chcp 65001 > nul
 
-SET PGPASSWORD=root
+:: Пути к бэкапам БД
+set PROLIFE_BACKUP_FILE=%BUILD_DIR%\ProLife\Tests\prolifetest.backup
+set LISA_BACKUP_FILE=%BUILD_DIR%\ProLife\Tests\lisatest.backup
+set PUMA_BACKUP_FILE=%BUILD_DIR%\ProLife\Tests\pumatest.backup
 
+:: Настройки PostgreSQL
+set PROLIFE_DB_NAME=prolifetest
+set LISA_DB_NAME=lisatest
+set PUMA_DB_NAME=pumatest
+set PG_USER=postgres
+set PG_HOST=localhost
+set PG_PORT=5432
+set PGPASSWORD=root
+
+:: Удаляем и создаем базы
 psql -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -c "DROP DATABASE IF EXISTS %PROLIFE_DB_NAME%;"  
-psql -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -c "CREATE DATABASE %PROLIFE_DB_NAME% WITH OWNER = %PG_USER%;"
-
+psql -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -c "CREATE DATABASE %PROLIFE_DB_NAME% WITH OWNER = %PG_USER%;"  
 psql -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -c "DROP DATABASE IF EXISTS %LISA_DB_NAME%;"  
-psql -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -c "CREATE DATABASE %LISA_DB_NAME% WITH OWNER = %PG_USER%;"
-
+psql -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -c "CREATE DATABASE %LISA_DB_NAME% WITH OWNER = %PG_USER%;"  
 psql -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -c "DROP DATABASE IF EXISTS %PUMA_DB_NAME%;"  
-psql -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -c "CREATE DATABASE %PUMA_DB_NAME% WITH OWNER = %PG_USER%;"
+psql -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -c "CREATE DATABASE %PUMA_DB_NAME% WITH OWNER = %PG_USER%;"  
 
-:: Запускаем pg_restore
-pg_restore -h localhost -p 5432 -U postgres -d %PROLIFE_DB_NAME% --verbose %PROLIFE_BACKUP_FILE%
-pg_restore -h localhost -p 5432 -U postgres -d %LISA_DB_NAME% --verbose %LISA_BACKUP_FILE%
-pg_restore -h localhost -p 5432 -U postgres -d %PUMA_DB_NAME% --verbose %PUMA_BACKUP_FILE%
+:: Восстанавливаем базы
+pg_restore -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -d %PROLIFE_DB_NAME% --verbose "%PROLIFE_BACKUP_FILE%"
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+pg_restore -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -d %LISA_DB_NAME% --verbose "%LISA_BACKUP_FILE%"
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+pg_restore -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -d %PUMA_DB_NAME% --verbose "%PUMA_BACKUP_FILE%"
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 timeout /t 5
 
-rem Запуск процессов в отдельных окнах
-start ""  %BUILD_DIR%\Lisa\Bin\Release_Qt6_VC17_x64\LisaServer.exe"
+:: Запуск серверов
+start "" "%BUILD_DIR%\Lisa\Bin\Release_Qt6_VC17_x64\LisaServer.exe"
 start "" "%BUILD_DIR%\ProLife\Bin\Release_Qt6_VC17_x64\ProLifeServer.exe"
 start "" "%BUILD_DIR%\Puma\Bin\Release_Qt6_VC17_x64\PumaServer.exe"
 
 timeout /t 5
 
 echo Newman started
-rem Запуск тестов в Postman с помощью Newman
-call newman run "%BUILD_DIR%\ProLife\Tests\postman_collection.json" --disable-unicode --suppress-exit-code 0
-echo Newman ended 
+call newman run "%BUILD_DIR%\ProLife\Tests\postman_collection.json" --disable-unicode --suppress-exit-code 1
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+echo Newman ended
 
-rem Завершение процессов
+:: Завершаем процессы
 taskkill /IM "LisaServer.exe" /F
 taskkill /IM "ProLifeServer.exe" /F
 taskkill /IM "PumaServer.exe" /F
 
-pause
+exit /b 0
