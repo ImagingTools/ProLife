6fe1d771231379bbdf5aa3857b4a14043ba14e45
Roman.Kucherenko <Roman.Kucherenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-06-29 16:15:22 +0000

    ProLife tests dockerfile corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@14197 21205d9f-8918-2240-a3d5-c39064a2c852

--- Tests/start.bat
+++ Tests/start.bat
@@ -5,6 +5,7 @@ echo === Waiting for PostgreSQL to start ===
 pg_isready -h localhost -p 5432 -U postgres
 IF ERRORLEVEL 1 (
     echo PostgreSQL is not ready yet. Waiting...
+    timeout /t 2 >nul
     GOTO waitloop
 )
 
@@ -21,6 +22,7 @@ set PG_HOST=localhost
 set PG_PORT=5432
 set PGPASSWORD=root
 
+rem === Initial restore ===
 psql -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -c "DROP DATABASE IF EXISTS %PROLIFE_DB_NAME%;"  
 psql -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -c "CREATE DATABASE %PROLIFE_DB_NAME% WITH OWNER = %PG_USER%;"  
 psql -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -c "DROP DATABASE IF EXISTS %LISA_DB_NAME%;"  
@@ -35,15 +37,48 @@ if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 pg_restore -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -d %PUMA_DB_NAME% --verbose "%PUMA_BACKUP_FILE%"
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
+rem === Установка приложений ===
 C:\installers\LisaServerInstall.exe /VERYSILENT /NORESTART
 C:\installers\PumaServerInstall.exe /VERYSILENT /NORESTART
 C:\installers\ProLifeServerInstall.exe /VERYSILENT /NORESTART
 
-echo === Starting applications ===
+rem === Запуск приложений ===
+start "" /B "C:\Program Files\ImagingTools\LisaServer\LisaServer.exe"
+start "" /B "C:\Program Files\ImagingTools\PumaServer\PumaServer.exe"
+start "" /B "C:\Program Files\ImagingTools\ProLifeServer\ProLifeServer.exe"
+
+rem === Выполнение API-тестов ===
+echo === Running Newman tests ===
+newman run C:\postman_collection.json
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+rem === Повторный откат баз данных ===
+echo === Resetting databases before GUI tests ===
+psql -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -c "DROP DATABASE IF EXISTS %PROLIFE_DB_NAME%;"  
+psql -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -c "CREATE DATABASE %PROLIFE_DB_NAME% WITH OWNER = %PG_USER%;"  
+psql -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -c "DROP DATABASE IF EXISTS %LISA_DB_NAME%;"  
+psql -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -c "CREATE DATABASE %LISA_DB_NAME% WITH OWNER = %PG_USER%;"  
+psql -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -c "DROP DATABASE IF EXISTS %PUMA_DB_NAME%;"  
+psql -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -c "CREATE DATABASE %PUMA_DB_NAME% WITH OWNER = %PG_USER%;"  
+
+pg_restore -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -d %PROLIFE_DB_NAME% --verbose "%PROLIFE_BACKUP_FILE%"
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+pg_restore -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -d %LISA_DB_NAME% --verbose "%LISA_BACKUP_FILE%"
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+pg_restore -h %PG_HOST% -p %PG_PORT% -U %PG_USER% -d %PUMA_DB_NAME% --verbose "%PUMA_BACKUP_FILE%"
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+rem === Перезапуск EXE ===
+taskkill /IM LisaServer.exe /F
+taskkill /IM PumaServer.exe /F
+taskkill /IM ProLifeServer.exe /F
 
 start "" /B "C:\Program Files\ImagingTools\LisaServer\LisaServer.exe"
 start "" /B "C:\Program Files\ImagingTools\PumaServer\PumaServer.exe"
 start "" /B "C:\Program Files\ImagingTools\ProLifeServer\ProLifeServer.exe"
 
-echo === Running Newman tests ===
-newman run C:\postman_collection.json
+rem === GUI-тесты Playwright ===
+echo === Running Playwright tests ===
+cd C:\gui
+npx playwright test
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
