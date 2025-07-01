c0135355b0e72cc87b3d932c06f9dcd9b9a96a2e
Roman.Kucherenko <Roman.Kucherenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-07-01 14:50:23 +0000

    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@14225 21205d9f-8918-2240-a3d5-c39064a2c852

--- Tests/frontend/start.sh
+++ Tests/frontend/start.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "=== Waiting for PostgreSQL to start ==="
-pg_ctlcluster 14 main start
-until pg_isready -h localhost -p 5432; do
-  echo "PostgreSQL is not ready yet. Waiting..."
-  sleep 2
-done
-
 ./reset_db.sh
 
 nohup /app/PumaServer &>/dev/null &
