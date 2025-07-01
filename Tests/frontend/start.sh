e8ccdc09877c9ab50a93bb60b04f16bb7c9542f4
Roman.Kucherenko <Roman.Kucherenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-07-01 14:34:14 +0000

    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@14223 21205d9f-8918-2240-a3d5-c39064a2c852

--- Tests/frontend/start.sh
+++ Tests/frontend/start.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "=== Waiting for PostgreSQL to start ==="
-
+pg_ctlcluster 14 main start
 until pg_isready -h localhost -p 5432 -U postgres; do
   echo "PostgreSQL is not ready yet. Waiting..."
   sleep 2
