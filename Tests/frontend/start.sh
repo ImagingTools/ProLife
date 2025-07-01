d7190767f312136142cab87827137c359c2fe835
Roman.Kucherenko <Roman.Kucherenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-07-01 15:03:40 +0000

    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@14227 21205d9f-8918-2240-a3d5-c39064a2c852

--- Tests/frontend/start.sh
+++ Tests/frontend/start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
+
 pg_ctlcluster 14 main start
+echo "local all postgres trust" > /etc/postgresql/14/main/pg_hba.conf
+echo "host all all 127.0.0.1/32 trust" >> /etc/postgresql/14/main/pg_hba.conf
+echo "host all all ::1/128 trust" >> /etc/postgresql/14/main/pg_hba.conf
+pg_ctlcluster 14 main restart
+
 psql -U postgres -c "ALTER USER postgres PASSWORD 'root';"
 
 ./reset_db.sh
