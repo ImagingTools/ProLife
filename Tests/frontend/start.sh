7eee1742228a80d21b48b01f4b88b25f8c34012b
Roman.Kucherenko <Roman.Kucherenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-07-01 14:58:23 +0000

    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@14226 21205d9f-8918-2240-a3d5-c39064a2c852

--- Tests/frontend/start.sh
+++ Tests/frontend/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+pg_ctlcluster 14 main start
+psql -U postgres -c "ALTER USER postgres PASSWORD 'root';"
 
 ./reset_db.sh
 
@@ -8,3 +10,5 @@ nohup /app/LisaServer &>/dev/null &
 nohup /app/ProLifeServer &>/dev/null &
 
 npx playwright test
+
+pg_ctlcluster 14 main stop
