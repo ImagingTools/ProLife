7c1d4c99041d7e91cabddcc385390e9ef9c439f8
Roman.Kucherenko <Roman.Kucherenko@21205d9f-8918-2240-a3d5-c39064a2c852>
2025-07-03 03:38:56 +0000

    ProLife Tests
    git-svn-id: http://b035a0a.online-server.cloud/svn/ITDevelopment@14273 21205d9f-8918-2240-a3d5-c39064a2c852

--- Tests/frontend/playwright.config.js
+++ Tests/frontend/playwright.config.js
@@ -12,33 +12,18 @@ module.exports = defineConfig({
     screenshot: 'only-on-failure',
     baseURL: 'http://localhost:7778',
   },
-  expect: {
-    toHaveScreenshot: { threshold: 0.3 },
-  },
   projects: [
     {
       name: 'authorized',
       testMatch: /tests\/auth\/.*\.test\.js/,
       use: {
         storageState: 'storageState.json',
-  	launchOptions: {
-    	args: [
-     	 '--font-render-hinting=none',     // Отключает хинтинг
-     	 '--disable-skia-runtime-opts',    // Отключает оптимизации Skia
-     	 '--disable-lcd-text',             // Отключает субпиксельный рендеринг
-    	],
-  },
       },
     },
     {
       name: 'guest',
       testIgnore: /tests\/auth\/.*\.test\.js/,
       use: {
-    	args: [
-     	 '--font-render-hinting=none',     // Отключает хинтинг
-     	 '--disable-skia-runtime-opts',    // Отключает оптимизации Skia
-     	 '--disable-lcd-text',             // Отключает субпиксельный рендеринг
-    	],
       },
     }
   ],
